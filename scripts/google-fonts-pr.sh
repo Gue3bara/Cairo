3ddc20e1175319b9a5a4d930d863601458be02eb
Eli Heuer <elih@protonmail.com>
2020-03-25 22:23:02 -0400

    Update build scripts for the new ExtraBlack weight

--- scripts/google-fonts-pr.sh
+++ scripts/google-fonts-pr.sh
@@ -11,11 +11,13 @@ echo "[INFO] Preparing a new $familyName pull request at $prDir"
 echo "[INFO] Moving variable fonts"
 cp fonts/vf/Cairo-VF.ttf ~/Google/fonts/ofl/cairo/Cairo[wght].ttf
 
-if [ "$build_static_fonts" = true ]; then
-  for font in fonts/ttf/Cairo-*; do
-    echo "[INFO] Moving static font $font ";
-    cp $font ~/Google/fonts/ofl/cairo/static/
-  done
-fi
+echo "[INFO] Moving Static fonts"
+cp fonts/ttf/Cairo-Black.ttf ~/Google/fonts/ofl/cairo/static/
+cp fonts/ttf/Cairo-Bold.ttf ~/Google/fonts/ofl/cairo/static/
+cp fonts/ttf/Cairo-ExtraBlack.ttf ~/Google/fonts/ofl/cairo/static/
+cp fonts/ttf/Cairo-ExtraLight.ttf ~/Google/fonts/ofl/cairo/static/
+cp fonts/ttf/Cairo-Light.ttf ~/Google/fonts/ofl/cairo/static/
+cp fonts/ttf/Cairo-Regular.ttf ~/Google/fonts/ofl/cairo/static/
+cp fonts/ttf/Cairo-SemiBold.ttf ~/Google/fonts/ofl/cairo/static/
 
 echo "[INFO] Done preparing $familyName pull request at $prDir"
