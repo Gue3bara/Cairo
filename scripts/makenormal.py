fe988e69dea3917427c9ec8e54d0526d0e137f80
Yanone <post@yanone.de>
2022-09-23 11:09:09 +0200

    Make sure that b/w font doesn't get COLR table by removing these parameters

--- scripts/makenormal.py
+++ scripts/makenormal.py
@@ -2,10 +2,17 @@ import glyphsLib
 import sys
 import copy
 
+# Remove all Color layers
 font = glyphsLib.GSFont(sys.argv[-2])
 for glyph in font.glyphs:
     for layer in copy.copy(glyph.layers):
         if layer.name.startswith("Color "):
             del glyph.layers[layer.layerId]
 
+# Remove color palettes so COLR table won't get created
+for master in font.masters:
+    if master.customParameters["Color Palettes"]:
+        del master.customParameters["Color Palettes"]
+
 font.save(sys.argv[-1])
+
